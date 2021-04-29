f45ac6d7a326567646a70178c16c21765db093ee
RAGHAVENDRA6021 <kadaparaghavendra1@gmail.com>
2021-04-29 13:01:59 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,34 +6,51 @@ class CartItem extends React.Component{
         super();
         this.state={
             price:99,
-            name:'Phone',
-            qty:1
+            name:'IPhone',
+            qty:1,
+            total:99
         }
         //this.increaseQuanity=this.increaseQuanity.bind(this);
     }
-    increaseQuanity = () =>{
-    this.state.qty+=1
+    increaseQuantity = () =>{
+        console.log(this);
+      var x=this.state.qty
+      x+=1
+      var sum=x*this.state.price
+    //set state form 1
+    this.setState({
+       qty: x,
+       total:sum
+    });
+   
+    }
+    decreaseQuantity =()=>{
+        var x=this.state.qty;
+        if(x>1){
+            this.setState({
+                qty:x-1,
+                total:this.state.total-this.state.price
+            })
+        }
     }
    render(){
-       const {price,name,qty}=this.state;
+       const {price,name,qty,total}=this.state;
        return (
            <div className="cart-item">
                <div className="left-block">
                   <img style={styles.image} src="./images/iphone.png"alt="no display"/>
                </div>
                <div className="right-block">
-                  <div style={productName.name}>{name}</div>
+                  <div style={styles.name}>{name}</div>
                   <div style={{color:'#777',fontWeight:500}}>Price : ${price}</div>
                   <div style={{color:'#777',fontWeight:500}}>Quantity : {qty}</div>
                   <div className="cart-item-actions">
                       {/*buttons*/}
                       <img alt="increase" className="action-icons" src="./images/add.png"onClick={this.increaseQuantity} />
-                      <img alt="decrease" className="action-icons" src="./images/minus.png"/>
+                      <img alt="decrease" className="action-icons" src="./images/minus.png" onClick={this.decreaseQuantity}/>
                       <img alt="delete" className="action-icons" src="./images/delete.png"/>
-
-
-
                   </div>
+                  <div style={styles.total}>Total :${total}</div>
                </div>
 
            </div>
@@ -46,13 +63,18 @@ const styles={
       width:110,
       borderRadius:6,
       background:'white',
-    }
-  }
-const productName={
+    },
     name:{
         fontSize:30,
         color:'#307fe2',
         fontWeight:800
+    },
+    total:{
+        fontSize:35,
+        color:'#373737',
+        fontWeight:600    
     }
-}
+  }
+
+
 export default CartItem;
