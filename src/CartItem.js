b8be49a88e541e7790acabf3af9c28c046086c1c
RAGHAVENDRA6021 <kadaparaghavendra1@gmail.com>
2021-04-29 10:13:06 +0530

    added state using this.state taking constructor

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,18 +2,32 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:99,
+            name:'Phone',
+            qty:1
+        }
+    }
    render(){
+       const {price,name,qty}=this.state;
        return (
            <div className="cart-item">
                <div className="left-block">
-                  <img style={styles.image} alt="no display"/>
+                  <img style={styles.image} src="./images/iphone.png"alt="no display"/>
                </div>
                <div className="right-block">
-                  <div style={productName.name}>Phone</div>
-                  <div style={{color:'#777'}}>$99</div>
-                  <div style={{color:'#777'}}>qty 1</div>
+                  <div style={productName.name}>{name}</div>
+                  <div style={{color:'#777',fontWeight:500}}>Price : ${price}</div>
+                  <div style={{color:'#777',fontWeight:500}}>Quantity : {qty}</div>
                   <div className="cart-item-actions">
                       {/*buttons*/}
+                      <img alt="increase" className="action-icons" src="./images/add.png"/>
+                      <img alt="decrease" className="action-icons" src="./images/minus.png"/>
+                      <img alt="delete" className="action-icons" src="./images/delete.png"/>
+
+
 
                   </div>
                </div>
@@ -27,7 +41,7 @@ const styles={
       height:110,
       width:110,
       borderRadius:6,
-      background:'#777'
+      background:'white',
     }
   }
 const productName={
